15733ef6ced98cd659a9dadaef7844e12281dc31
Eduardo-Desplanches <eduardodasantos34@gmail.com>
2025-04-09 16:55:07 -0300

    refined code

--- src/desafio2/Desafio2.java
+++ src/desafio2/Desafio2.java
@@ -13,15 +13,18 @@ public class Desafio2 {
             System.out.println("\n===== Menu Ninja =====");
             System.out.println("1. Cadastrar Ninja");
             System.out.println("2. Listar Ninjas");
-            System.out.println("3. Sair");
+            System.out.println("3. Deletar Ninjas");
+            System.out.println("4. Sair");
             System.out.print("Escolha uma opção: ");
             int numero = scanner.nextInt();
+            scanner.nextLine();
 
             switch (numero){
                 case 1:
                     for (int i = 0; i < ninjas.length; i++) {
                         System.out.println("Escreva nome de ninja:");
                         ninjas[i] = scanner.nextLine();
+                        System.out.println("Ninja cadastrado com sucesso");
                     }
                     break;
                 case 2:
@@ -34,13 +37,26 @@ public class Desafio2 {
                     }
                     break;
                 case 3:
+                    System.out.println("Digite a posição do ninja que deseja retirar. De 1 a 5");
+                    int posicao;
+                    do {
+                        posicao = scanner.nextInt();
+                        if (posicao < 1|| posicao > ninjas.length ){
+                            System.out.println("Posição inválida. Tente novamente");
+                        }
+                    }while(posicao < 1 || posicao > ninjas.length);
+                    ninjas[posicao-1] = null;
+                    System.out.println("Posição retirada com sucesso");
+                    break;
+                case 4:
+                    System.out.println("Finalizando...");
                     condicao = false;
                     break;
                 default:
-                    System.out.println("Escreva com as opções acima e tente novamente.");
+                    System.out.println("Opção inválida, tente novamente.");
 
             }
-        }while(!condicao);
+        }while(condicao);
 
         scanner.close();
     }
